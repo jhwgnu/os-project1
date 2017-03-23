2051549ad41083c67df79ab0c50067b3e680959b
jhwgnu <jhwon11@snu.ac.kr>
2017-03-23 14:31:33 +0900

    initialization

--- linux-3.10-artik/include/linux/syscalls.h
+++ linux-3.10-artik/include/linux/syscalls.h
@@ -847,5 +847,5 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 			 unsigned long idx1, unsigned long idx2);
 asmlinkage long sys_finit_module(int fd, const char __user *uargs, int flags);
-asmlinkage long sys_mycall(struct prinfo *buf, int *nr);
+asmlinkage long sys_ptree(void);
 #endif
--- linux-3.10-artik/kernel/ptree.c
+++ linux-3.10-artik/kernel/ptree.c
@@ -1,1 +1,9 @@
-// ptree systemcall
+#include <linux/unistd.h>
+#include <linux/errno.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+
+asmlinkage long sys_ptree(void){
+    printk("<0>Hello, I'm in Kernel");
+    return 0;
+}
